a4fb73df441f44e945b8f4bb71682e3a38742669
Matthew Hruz <hruzm@charter.net>
2012-10-18 17:23:31 -0500

    why must we always have a reason for a commit?

--- Phase10/src/phase10/gui/GuiManager.java
+++ Phase10/src/phase10/gui/GuiManager.java
@@ -1,20 +1,20 @@
 package gui;
 
-import main.GameManager;
+//import main.GameManager;
 
 public class GuiManager {
 	
 	private GameFrame mainframe;
-	public GameManager mainManager;
+//	public GameManager mainManager;
 
 	
 	public static void main(String[] args) {
 
 	}
 	
-	public GuiManager(GameManager m) {
-		mainManager = m; //passes a reference from the game manager into the GUI manager
-	}
+//	public GuiManager(GameManager m) {
+//		mainManager = m; //passes a reference from the game manager into the GUI manager
+//	}
 	
 	public void initGame() {
 	}
